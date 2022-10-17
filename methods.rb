90543251804c4e4c9c98c8cff9d057b5720f9e84
+ <+>
2022-10-17 13:21:02 +0300

    commit tree c94f26903b46423fe36fe7d0cea1a17e2952cbf0

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+
+def greet_programmer
+    puts 'Hello, programmer!'
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default(name='programmer')
+    puts "Hello, #{name}!"
+end 
+greet_with_default "Naureen"
+
+def add(num1,num2)
+    return num1 + num2
+end
+add(2, 5)
+
+def halve(y)
+    if y.class != Integer
+    return nil
+end
+return y/2
+end
+halve(4)
+ 
